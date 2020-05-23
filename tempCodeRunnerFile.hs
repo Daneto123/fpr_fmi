9e8e943e001551ba2ee1a329ce12c3b05b3ba63d
Jordan <33426427+Daneto123@users.noreply.github.com>
2020-05-23 20:12:56 +0300

    Delete tempCodeRunnerFile.hs

--- tempCodeRunnerFile.hs
+++
@@ -1,5 +0,0 @@
-t1 :: BTree Char                                 --   a
--- t1 = Node 'a' (Node 'c' (Node 'f' Empty Empty)   --  / \
---                         (Node 'd' Empty Empty))  --  c  b
---                         (Node 'b' Empty          -- / \  \
---                 (Node 'e' Empty Empty))          -- f  d  e 
